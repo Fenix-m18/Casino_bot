19d7f14a269861d2872869acbd745063081e2166
Fenix-m18 <58095963+Fenix-m18@users.noreply.github.com>
2023-08-05 00:08:06 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 #Трогать, значения можно менять
-admin = [285060597] #ID админа
-support_name = '@botYanis' #Имя саппорта, будет выводиться при нажатии кнопки "поддержка"
-token = '5675159912:AAHgN_mzWdbiNtQIq33yEJ2AjxJtIHyEGDE' #Токен бота
+admin = [1825534107] #ID админа
+support_name = 'https://t.me/azimut_support_bot' #Имя саппорта, будет выводиться при нажатии кнопки "поддержка"
+token = '6344190414:AAFQF2HcT8zMm-WeN9VxtJ4ZRzOe63XUylc' #Токен бота
 client_id = "" #client_id, получается при создании приложения Юмани
 number_qiwi = '+79218681790' #Номер вашего QIWI кошелька
 token_qiwi = '' #Токен киви
